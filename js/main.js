0febbfdfe9ffe6a81e5480b65d2095191ac7983c
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-09-13 03:17:15 -0300

    Primeira Aula

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Rodrigo Telles de Abreu";
+var idade = 40;
+var idade2 = 2;
+var frase = "Japão é o melhor time do mundo"
+/* alert(idade + idade2);*/
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase)
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+/*alert(frase.replace("Japão", "Brasil"))*/
